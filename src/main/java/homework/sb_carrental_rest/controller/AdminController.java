a9771e07645bfbed914d00d7f0c0419f7d63aee8
KatalinDorman <dkata75@gmail.com>
2024-10-11 10:51:05 +0200

    refactor

--- src/main/java/homework/sb_carrental_rest/controller/AdminController.java
+++ src/main/java/homework/sb_carrental_rest/controller/AdminController.java
@@ -4,81 +4,44 @@ import homework.sb_carrental_rest.dto.AdminDto;
 import homework.sb_carrental_rest.dto.CarDto;
 import homework.sb_carrental_rest.service.AdminService;
 import lombok.AllArgsConstructor;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class AdminController {
 
     private AdminService adminService;
 
     @GetMapping("/admin")
-    public String loadAdminPage(Model model) {
+    public AdminDto loadAdminPage() {
 
-        AdminDto adminDto = adminService.getAdminDto();
-
-        model.addAttribute("adminDto", adminDto);
-
-        return "admin.html";
+        return adminService.getAdminDto();
     }
 
     @PostMapping("/admin/newcar")
-    public String saveNewCar(Model model,
-                             @RequestParam(name = "type") String type,
+    public CarDto saveNewCar(@RequestParam(name = "type") String type,
                              @RequestParam(name = "isAvailable") String isAvailable,
                              @RequestParam(name = "price") int price,
                              @RequestParam("file") MultipartFile file) {
-        String returnPage = "";
 
-        CarDto carDto = adminService.prepareCarDto(type, isAvailable, price, file);
-
-        if (!carDto.isNewCarSaved()) {
-            loadAdminPage(model);
-            returnPage = "admin.html";
-        } else {
-            returnPage = "newcar.html";
-        }
-
-        model.addAttribute("carDto", carDto);
-
-        return returnPage;
+        return adminService.prepareCarDto(type, isAvailable, price, file);
     }
 
     @GetMapping("/admin/editcar")
-    public String loadEditPage(Model model,
-                               @RequestParam(name = "carId") int id) {
+    public CarDto loadEditPage(@RequestParam(name = "carId") int id) {
 
-        CarDto carDto = adminService.getCarDtoById(id);
-
-        model.addAttribute("carDto", carDto);
-
-        return "editcar.html";
+        return adminService.getCarDtoById(id);
     }
 
     @PostMapping("/admin/editcar/commit")
-    public String saveEditedCar(Model model,
-                                @RequestParam(name = "carId") int id,
+    public CarDto saveEditedCar(@RequestParam(name = "carId") int id,
                                 @RequestParam(name = "type") String type,
                                 @RequestParam(name = "isAvailable") String isAvailable,
                                 @RequestParam(name = "price") int price,
                                 @RequestParam("file") MultipartFile file) {
-        String returnPage = "";
 
-        CarDto carDto = adminService.updateCarDto(id, type, isAvailable, price, file);
-
-        if (!carDto.isNewCarSaved()) {
-            returnPage = "editcar.html";
-        } else {
-            returnPage = "newcar.html";
-        }
-
-        model.addAttribute("carDto", carDto);
-
-        return returnPage;
+        return adminService.updateCarDto(id, type, isAvailable, price, file);
     }
 }
--- src/main/java/homework/sb_carrental_rest/controller/CarRentalController.java
+++ src/main/java/homework/sb_carrental_rest/controller/CarRentalController.java
@@ -4,65 +4,41 @@ import homework.sb_carrental_rest.dto.CarDtoList;
 import homework.sb_carrental_rest.dto.ReservationDto;
 import homework.sb_carrental_rest.service.CarRentalService;
 import lombok.AllArgsConstructor;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class CarRentalController {
 
     private CarRentalService carRentalService;
 
-    @GetMapping("/")
-    public String loadStartPage() {
-        return "index.html";
-    }
-
     @GetMapping("/cars")
-    public String loadAvailableCars(Model model,
-                                    @RequestParam(name = "startDate") LocalDate startDate,
-                                    @RequestParam(name = "endDate") LocalDate endDate) {
+    public CarDtoList loadAvailableCars(@RequestParam(name = "startDate") LocalDate startDate,
+                                        @RequestParam(name = "endDate") LocalDate endDate) {
 
-        CarDtoList carDtoList = carRentalService.getAvailableCarsForChosenPeriod(startDate, endDate);
-
-        model.addAttribute("carDtoList", carDtoList);
-
-        return "cars.html";
+        return carRentalService.getAvailableCarsForChosenPeriod(startDate, endDate);
     }
 
     @GetMapping("/cars/startres")
-    public String startReservation(Model model,
-                                   @RequestParam(name = "startDate") LocalDate startDate,
-                                   @RequestParam(name = "endDate") LocalDate endDate,
-                                   @RequestParam(name = "carId") int carId) {
+    public ReservationDto startReservation(@RequestParam(name = "startDate") LocalDate startDate,
+                                           @RequestParam(name = "endDate") LocalDate endDate,
+                                           @RequestParam(name = "carId") int carId) {
 
-        ReservationDto reservationDto = carRentalService.prepareReservationDto(startDate, endDate, carId);
-
-        model.addAttribute("reservationDto", reservationDto);
-
-        return "reservation.html";
+        return carRentalService.prepareReservationDto(startDate, endDate, carId);
     }
 
     @PostMapping("/cars/endres")
-    public String endReservation(Model model,
-                                 @RequestParam(name = "carId") int carId,
-                                 @RequestParam(name = "startDate") LocalDate startDate,
-                                 @RequestParam(name = "endDate") LocalDate endDate,
-                                 @RequestParam(name = "name") String name,
-                                 @RequestParam(name = "email") String email,
-                                 @RequestParam(name = "address") String address,
-                                 @RequestParam(name = "phone") String phone) {
+    public ReservationDto endReservation(@RequestParam(name = "carId") int carId,
+                                         @RequestParam(name = "startDate") LocalDate startDate,
+                                         @RequestParam(name = "endDate") LocalDate endDate,
+                                         @RequestParam(name = "name") String name,
+                                         @RequestParam(name = "email") String email,
+                                         @RequestParam(name = "address") String address,
+                                         @RequestParam(name = "phone") String phone) {
 
-        ReservationDto reservationDto = carRentalService.getReservationDto(carId, startDate, endDate, name, email,
-                address, phone);
-
-        model.addAttribute("reservationDto", reservationDto);
-
-        return "result.html";
+        return carRentalService.getReservationDto(carId, startDate, endDate, name, email, address, phone);
     }
 }
